f2b7606f9e23777249b1ef68087d5269d4a4d1fc
CoolGuy158 <ernestoeduardogallego@gmail.com>
2025-11-11 15:04:49 +1200

    BootOS ver1.3
    Added paint.exe

--- BootOS.py
+++ BootOS.py
@@ -192,7 +192,7 @@ def BootVer():
 
     make_draggable(VerWindows)
 
-    label4 = tk.Label(VerWindows, text="BootOS Ver 1.2", bg="white", fg="black", font=("Arial", 12, "bold"))
+    label4 = tk.Label(VerWindows, text="BootOS Ver 1.3", bg="white", fg="black", font=("Arial", 12, "bold"))
     label4.place(relx=0.5, y=250, anchor="center")
 
     label5 = tk.Label(
@@ -221,6 +221,42 @@ def notepad():
     text_box.place(x=10, y=10, width=480, height=380)
 
     cancel = tk.Button(notewindow, text="x", command=notewindow.destroy, fg="red", height=1, font=("Arial", 10))
+    cancel.place(relx=1, rely=0, anchor="ne")
+
+def paint():
+    root_x = root.winfo_x() + (root.winfo_width() // 2) - 300
+    root_y = root.winfo_y() + (root.winfo_height() // 2) - 300
+    paintwindows = tk.Frame(root, bg="black", height=400, width=500, borderwidth=2, relief="ridge")
+    paintwindows.lift()
+    make_draggable(paintwindows)
+    paintwindows.place(x=root_x, y=root_y)
+    canvas = tk.Canvas(paintwindows, width=500, height=370, bg="white")
+    canvas.place(x=0, y=30)
+
+    colors = ["black", "red", "blue", "green"]
+    current_color_index = 0
+
+
+    def paint1(event):
+        x1, y1 = (event.x - 2), (event.y - 2)
+        x2, y2 = (event.x + 2), (event.y + 2)
+        canvas.create_oval(x1, y1, x2, y2, fill=colors[current_color_index], outline=colors[current_color_index])
+    def toggle_color(event):
+        nonlocal current_color_index
+        current_color_index = (current_color_index + 1) % len(colors)
+        paintwindows.config(bg=colors[current_color_index])
+
+    canvas.bind("<Button-3>", toggle_color)
+
+    canvas.bind("<B1-Motion>", paint1)
+
+    def clear_canvas():
+        canvas.delete("all")
+
+    clear_btn = tk.Button(paintwindows, text="Clear", command=clear_canvas)
+    clear_btn.place(relx=0.9, rely=0, anchor="ne")
+
+    cancel = tk.Button(paintwindows, text="x", command=paintwindows.destroy, fg="red", height=1, font=("Arial", 10))
     cancel.place(relx=1, rely=0, anchor="ne")
 
 
@@ -287,6 +323,9 @@ def show_os():
 
     notepad1 = tk.Button(root, text="Notepad.exe", command=notepad,)
     notepad1.place(x=28, y=156,)
+
+    paintapp = tk.Button(root, text="Paint.exe", command=paint)
+    paintapp.place(x=28, y=206,)
 
 #boot Screen
 screen_width = root.winfo_screenwidth()
